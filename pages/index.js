bb12f57ee8d082291af796ff8394f7cf1d13eae9
Brian Holt <btholt@gmail.com>
2022-02-28 15:58:51 -0600

    favicons

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,28 @@ export default function Lessons({ sections }) {
           content={`${process.env.BASE_URL}/images/social-share-cover.jpg`}
         ></meta>
         <meta name="twitter:card" content="summary_large_image"></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${process.env.BASE_URL}/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${process.env.BASE_URL}/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${process.env.BASE_URL}/favicon-16x16.png`}
+        ></link>
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href={`${process.env.BASE_URL}/favicon.ico`}
+        ></link>
       </Head>
       <div>
         <div className="jumbotron">
